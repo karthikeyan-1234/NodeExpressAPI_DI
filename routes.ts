06548e47f9941e735b0ec0a437080e5e44bedc8e
karthikeyan-1234 <87603111+karthikeyan-1234@users.noreply.github.com>
2023-04-13 08:49:22 +0530

    Few syntax changes

--- routes.ts
+++ routes.ts
@@ -4,11 +4,10 @@ import container from './container';
 const router = express.Router();
 const postService = container.resolve('IPostService');
 
-
 router.get('/', (req, res) => {
     const result = postService.findPost(1);
     console.log(result);
     res.send('Hello World  !!!!')
   })
 
-  export default router;
+export default router;
